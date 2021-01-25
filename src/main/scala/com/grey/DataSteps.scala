35ea37108736842ecea5d83511ebf400544c56f5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-25 11:50:51 +0000

    Update src/main/scala/com/grey/DataSteps.scala

--- src/main/scala/com/grey/DataSteps.scala
+++ src/main/scala/com/grey/DataSteps.scala
@@ -2,18 +2,35 @@ package com.grey
 
 import com.grey.directories.LocalSettings
 import com.grey.inspectors.InspectArguments
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.upper
+import org.apache.spark.storage.StorageLevel
 
 class DataSteps(spark: SparkSession) {
 
   val localSettings = new LocalSettings()
 
+
   def dataSteps(parameters: InspectArguments.Parameters): Unit = {
 
-    val acquisitions: DataFrame = new com.grey.sources.Read(spark = spark).
+    import spark.implicits._
+
+    // Data
+    val (acquisitionsFrame: DataFrame, acquisitionsSet: Dataset[Row]) = new com.grey.sources.Read(spark = spark).
       read(src = "acquisitions.csv", database = "crunchbase", parameters = parameters)
 
-    acquisitions.show()
+    // Persistence
+    acquisitionsFrame.persist(StorageLevel.MEMORY_ONLY)
+    acquisitionsSet.persist(StorageLevel.MEMORY_ONLY)
+
+    // Temporary table
+    acquisitionsFrame.createOrReplaceTempView("acquisitions")
+
+    // Previews
+    acquisitionsFrame.select($"uuid", $"name", upper($"type").as("type"),
+      $"permalink", $"price", $"price_currency_code").show(9)
+    acquisitionsSet.select($"uuid", $"name", upper($"type").as("type"),
+      $"permalink", $"price", $"price_currency_code").show(9)
 
   }
 
