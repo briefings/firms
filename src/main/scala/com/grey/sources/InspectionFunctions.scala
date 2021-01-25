d702a71b2de1bb27079166eca8147340080f777c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-25 18:03:07 +0000

    Add src/main/scala/com/grey/sources/InspectionFunctions.scala

---
+++ src/main/scala/com/grey/sources/InspectionFunctions.scala
@@ -0,0 +1,27 @@
+package com.grey.sources
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, upper}
+
+class InspectionFunctions {
+
+  def acquisitions(data: DataFrame): DataFrame = {
+
+    // The aim herein is to ensure that the contents of the listed fields are upper case text
+    val reset = List("type", "acquiree_country_code", "acquiree_state_code",
+      "acquirer_country_code", "acquirer_state_code", "acquisition_type")
+
+    // Mutable variable form
+    var frame = data
+
+    // Upper case
+    reset.foreach { name =>
+      val temporary: String = name + "_temporary"
+      frame = frame.withColumn(temporary, upper(col(name))).drop(col(name))
+        .withColumnRenamed(existingName = temporary, newName = name)
+    }
+    frame
+
+  }
+
+}
