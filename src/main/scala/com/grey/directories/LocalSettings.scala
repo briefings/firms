b3f29ee303c34eb304a23f7847564847014b757c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-25 08:55:18 +0000

    Update src/main/scala/com/grey/directories/LocalSettings.scala

--- src/main/scala/com/grey/directories/LocalSettings.scala
+++ src/main/scala/com/grey/directories/LocalSettings.scala
@@ -1,12 +1,17 @@
 package com.grey.directories
 
+import java.nio.file.Paths
+
 class LocalSettings {
 
   // Of environment
   val projectDirectory: String = System.getProperty("user.dir")
   val sep: String = System.getProperty("file.separator")
 
-  // Resources
-  val resourcesDirectory = s"$projectDirectory${sep}src${sep}main${sep}resources$sep"
+  // Base names
+  val names = List("src", "main", "resources")
+
+  // val resourcesDirectory = s"$projectDirectory${sep}src${sep}main${sep}resources$sep"
+  val resourcesDirectory: String = Paths.get(projectDirectory, names: _* ).toString + sep
 
 }
