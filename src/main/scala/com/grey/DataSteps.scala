79ff3ec2618fd88ab4585c99f61332aea07e7045
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-26 12:10:10 +0000

    Update src/main/scala/com/grey/DataSteps.scala

--- src/main/scala/com/grey/DataSteps.scala
+++ src/main/scala/com/grey/DataSteps.scala
@@ -2,32 +2,40 @@ package com.grey
 
 import com.grey.directories.LocalSettings
 import com.grey.inspectors.InspectArguments
+import com.grey.sources.DataRead
 import org.apache.spark.sql._
 import org.apache.spark.storage.StorageLevel
 
 class DataSteps(spark: SparkSession) {
 
-  val localSettings = new LocalSettings()
-
+  val dataRead = new DataRead(spark = spark)
 
   def dataSteps(parameters: InspectArguments.Parameters): Unit = {
 
     import spark.implicits._
 
-    // Data
-    val (acquisitionsFrame: DataFrame, acquisitionsSet: Dataset[Row]) = new com.grey.sources.Read(spark = spark).
-      read(src = "acquisitions.csv", database = "crunchbase", parameters = parameters)
+    // Acquisitions
+    val (acquisitionsFrame: DataFrame, acquisitionsSet: Dataset[Row]) = dataRead.
+      dataRead(src = "acquisitions.csv", database = "crunchbase", parameters = parameters)
 
-    // Persistence
+    // ... Persistence
     acquisitionsFrame.persist(StorageLevel.MEMORY_ONLY)
     acquisitionsSet.persist(StorageLevel.MEMORY_ONLY)
     acquisitionsFrame.createOrReplaceTempView("acquisitions")
 
+    // Companies
+    val (companiesFrame: DataFrame, companiesSet: Dataset[Row]) = dataRead
+      .dataRead(src = "companies.csv", database = "crunchbase", parameters = parameters)
+
+    // ... Persistence
+    companiesFrame.cache()
+    companiesSet.cache()
+    companiesFrame.createOrReplaceTempView("companies")
+
     // Previews
-    acquisitionsFrame.select($"uuid", $"name", $"type",
-      $"permalink", $"acquisition_type", $"price", $"price_currency_code").show(9)
-    acquisitionsSet.select($"uuid", $"name", $"type",
-      $"permalink", $"acquisition_type", $"price", $"price_currency_code").show(9)
+    acquisitionsSet.select($"acquiree_uuid", $"acquiree_name", $"acquirer_uuid", $"acquirer_name",
+      $"acquirer_country_code", $"acquisition_type", $"price", $"price_currency_code").show(5)
+    companiesSet.select($"uuid", $"name", $"short_description", $"country_code").show(5)
 
   }
 
