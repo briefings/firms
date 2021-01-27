77ecbb1dc4a582f8064f4372c23ab56b1cec5677
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-27 01:05:33 +0000

    Update src/main/scala/com/grey/sql/OuterJoin.scala

--- src/main/scala/com/grey/sql/OuterJoin.scala
+++ src/main/scala/com/grey/sql/OuterJoin.scala
@@ -9,12 +9,13 @@ class OuterJoin(spark: SparkSession) {
     println("\n\n SQL Outer Join Types")
 
     // left join
-    val leftJoin: DataFrame = spark.sql("SELECT companies.uuid, companies.name, " +
-      "investors.name as investor_name, investors.type, investors.rank, investors.created_at " +
-      "FROM companies LEFT OUTER JOIN investors ON (companies.uuid = investors.uuid) " +
+    val leftJoin: DataFrame = spark.sql("SELECT investors.uuid, investors.name, investors.type, " +
+      "investors.investment_count, investors.created_at, companies.country_code, companies.short_description " +
+      "FROM investors LEFT OUTER JOIN companies ON (investors.uuid = companies.uuid) " +
+      "WHERE investment_count IS NOT NULL " +
       "ORDER BY investors.created_at ASC NULLS LAST")
 
-    println("left join: " + leftJoin.count())
+    println("investors that have one or more investments on record: " + leftJoin.count())
     leftJoin.show(5)
 
 
